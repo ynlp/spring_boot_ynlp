30b9a457b92462fe1be4d152c45aea1f74f47b69
ynlp <wangliangduan@foxmail.com>
2018-05-22 16:05:56 +0800

    Initial commit

---
+++ src/main/java/com/example/ynlp/YnlpApplication.java
@@ -0,0 +1,12 @@
+package com.example.ynlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnlpApplication.class, args);
+	}
+}
